f3c958cf7ee8aba6907dbed3a65842ab7df0276c
LucasManuelRodriguez <rodriguez.m.lucas@gmail.com>
2021-10-25 06:13:16 -0300

    First exercise

---
+++ main.js
@@ -0,0 +1,28 @@
+// 3. Teniendo un cuadrado de color rojo en el documento de HTML:
+// Desarrollar una función que, al hacer click en el mismo, éste se transforme en una círculo de color verde
+// 4. Teniendo un input de texto y un botón:
+// Desarrollar una función que al hacer click en él, escriba el contenido (value) del input como un título en el documento.
+// 5. Desarrollar una función que escriba en la consola del navegador cada item de una lista de la compra.
+// 6. Desarrollar una función que dibuje una 'Card' de una película en el documento usando un objeto javascript como datos.
+
+
+
+// 1. Desarrollar una función que escriba en la consola del navegador cada item de una lista de la compra.
+
+let ferreteria = ["tornillos","martillo","clavo","tarugo","oring","arandela"];
+
+function printlist(){
+    ferreteria.forEach(e=>{
+        console.log(e)
+    })
+}
+printlist()
+//-----------------------------------------------------------------------------------------------------------
+
+// 2. Desarrollar una función que escriba un Título h1 en el documento HTML.
+
+
+
+
+
+
